d6d4f1fb4c9945dd08fff2c25a43fe774c379eda
LudmilaAzevedo <161759503+LudmilaAzevedo@users.noreply.github.com>
2025-08-08 14:30:09 -0300

    versionando

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { SERVER_ROUTES } from "./appConfig.js";
+import UsuarioController from "./controller/UsuarioController.js";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ mensagem: "Rota padrão" })
+});
+
+//CRUD Usuario
+router.get(SERVER_ROUTES.LISTAR_USUARIO, UsuarioController.todos
+
+    
+);
+
+export { router }
